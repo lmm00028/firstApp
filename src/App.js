1e54f993b228a6a397dc25e1a9ae9730ddc69ea5
your user name <your email>
2023-10-17 19:30:55 -0400

    intial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>CS230L</h1>
+     <h2>Section-001</h2>
+     <p>WVU ID: 800370291</p>
+     <p>Hi I am Luke</p>
+    </div>
+  );
+}
+
+export default App;
